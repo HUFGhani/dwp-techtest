bb57781b77c0ba9b730844808c15b5e44034cc27
Hamza Umar Farooq Ghani <hamzaumarfarooqghani@rentalcars.com>
2020-08-23 20:43:26 +0100

    starting point

---
+++ src/main/java/io/gitub/hufghani/dwptechtest/DwpTechtestApplication.java
@@ -0,0 +1,12 @@
+package io.gitub.hufghani.dwptechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwpTechtestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DwpTechtestApplication.class, args);
+  }
+}
---
+++ src/test/java/io/gitub/hufghani/dwptechtest/DwpTechtestApplicationTests.java
@@ -0,0 +1,11 @@
+package io.gitub.hufghani.dwptechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwpTechtestApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
